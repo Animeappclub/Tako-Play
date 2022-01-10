df775c8dde054b5af2db9490cf5f7ffd6b9f452b
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2022-01-11 03:15:01 +0530

    Update tako_theme.dart

--- lib/theme/tako_theme.dart
+++ lib/theme/tako_theme.dart
@@ -1,57 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../utils/constants.dart';
 
 class TakoTheme {
   static TextTheme darkTextTheme = TextTheme(
-    bodyText1: GoogleFonts.questrial(
-      fontSize: 15.0,
-      fontWeight: FontWeight.w600,
-      color: Colors.white,
-    ),
-    bodyText2: GoogleFonts.questrial(
+    bodyText1: GoogleFonts.roboto(
       fontSize: 15.0,
       fontWeight: FontWeight.normal,
       color: Colors.white,
     ),
-    subtitle1: GoogleFonts.questrial(
+    subtitle1: GoogleFonts.roboto(
       fontSize: 15.0,
       fontWeight: FontWeight.w700,
       color: Colors.white,
     ),
-    subtitle2: GoogleFonts.questrial(
+    subtitle2: GoogleFonts.roboto(
       fontSize: 17.0,
       color: Colors.white,
     ),
-    headline1: GoogleFonts.questrial(
+    headline1: GoogleFonts.roboto(
       fontSize: 31.0,
       fontWeight: FontWeight.bold,
       color: Colors.white,
     ),
-    headline2: GoogleFonts.questrial(
+    headline2: GoogleFonts.roboto(
       fontSize: 20.0,
       fontWeight: FontWeight.w700,
       color: Colors.white,
     ),
-    headline3: GoogleFonts.questrial(
+    headline3: GoogleFonts.roboto(
       fontSize: 19.0,
       fontWeight: FontWeight.w600,
       color: Colors.white,
       decoration: TextDecoration.none,
     ),
-    headline4: GoogleFonts.questrial(
+    headline4: GoogleFonts.roboto(
       fontSize: 22.0,
       fontWeight: FontWeight.w700,
       color: Colors.white,
       decoration: TextDecoration.none,
     ),
-    headline5: GoogleFonts.questrial(
+    headline5: GoogleFonts.roboto(
       fontSize: 19.0,
       fontWeight: FontWeight.w700,
       color: Colors.white,
       decoration: TextDecoration.none,
     ),
-    headline6: GoogleFonts.questrial(
+    headline6: GoogleFonts.roboto(
       fontSize: 19.0,
       fontWeight: FontWeight.w600,
       color: Colors.white,
@@ -79,7 +75,6 @@ class TakoTheme {
         unselectedItemColor: tkGrey,
         backgroundColor: tkDarkBlue,
       ),
-      
       checkboxTheme: CheckboxThemeData(
         checkColor: MaterialStateProperty.all(Colors.black),
         fillColor: MaterialStateProperty.all(tkLightGreen.withAlpha(205)),
